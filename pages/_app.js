34598ae11edbd8362eb59bbbae795e7edb46dfae
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2021-12-29 14:55:29 +0900

    Home/About 링크 안 보이는 문제 해결 - pages/_app.tsx 파일 삭제

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import NavBar from "../components/NavBar";
 import "../styles/globals.css";
+import NavBar from "./../components/NavBar";
 
 export default function App({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="active">Hi</h1>
+      <h1 className="active">Hello</h1>
     </div>
   );
 }
