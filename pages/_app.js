bb80893f206805039b3bcec0bcdb7bdd97d5ef00
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2021-12-31 23:14:32 +0900

    Patterns

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
+import Layout from "../components/Layout";
 import "../styles/globals.css";
-import NavBar from "./../components/NavBar";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-    </>
+      <footer></footer>
+    </Layout>
   );
 }
